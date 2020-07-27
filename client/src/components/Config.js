ba8c26cd4209fc9e8c83c256a2801cb652dbba48
groo83 <pjy3696@naver.com>
2020-07-27 22:22:09 +0900

    first commit, api url config setting

---
+++ client/src/components/Config.js
@@ -0,0 +1,10 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '22b1464a327e0fde01d47a28a0fa6f73';
+
+
+export const IMAGE_BASE_URL ='http://image.tmdb.org/t/p/';
+
